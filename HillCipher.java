1806e59e21ca48e5831a4e6a8888a4b21388477d
Kabe-Innovates <kabe.innovates@gmail.com>
2026-01-30 22:28:40 +0530

    feat : Experiment Completed

--- HillCipher.java
+++ HillCipher.java
@@ -1,33 +1,3 @@
-# Ex-3 IMPLEMENTATION OF HILL CIPHER
- 
-## AIM
-To write a C program to implement the hill cipher substitution techniques.
-
-## DESCRIPTION:
-
-Each letter is represented by a number modulo 26. Often, the simple scheme  
-A = 0, B = 1, ... Z = 25 is used, but this is not an essential feature of the cipher.
-
-To encrypt a message, each block of *n* letters is multiplied by an invertible  
-*n × n* matrix modulo 26.
-
-To decrypt the message, each block is multiplied by the inverse of the matrix  
-used for encryption.
-
-The matrix used for encryption is the cipher key, and it should be chosen  
-randomly from the set of invertible *n × n* matrices (modulo 26).
-
-
-## ALGORITHM:
-
-STEP-1: Read the plain text and key from the user. <br>
-STEP-2: Split the plain text into groups of length three. <br>
-STEP-3: Arrange the keyword in a 3*3 matrix.<br>
-STEP-4: Multiply the two matrices to obtain the cipher text of length three. <br>
-STEP-5: Combine all these groups to get the complete cipher text.<br>
-
-## PROGRAM 
-```java
 public class HillCipher {
 
     static int[][] keyMat = {
@@ -120,11 +90,3 @@ public class HillCipher {
     }
 }
 
-
-```
-
-## OUTPUT
-![alt text](image.png)
-
-## RESULT
- Thus the implementation of ceasar cipher had been executed successfully.
